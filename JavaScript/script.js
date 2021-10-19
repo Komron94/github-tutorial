8b80eff49821b064d793e8a1e5fbdb2ef3d2da4e
Komron <example@gmail.com>
2021-10-19 10:45:45 +0500

    first commit

---
+++ JavaScript/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// console.log(2 && 1 && null && 0 && undefined);
+// console.log(2 || 1 && 5 || 0 || undefined);
+
+// const person = {
+//   firstName: "Komron",
+//   lastName: "Axmedov",
+//   age: 26,
+//   job: "Full-Stack JS Developer",
+//   isMaried: false,
+// };
+// console.log(person.age);
+
+const answers = [];
+answers[0] = prompt("Ismiz mima");
+answers[1] = prompt("Familiya nima");
+answers[2] = prompt("Yoshiz nechida");
+document.write(answers);
